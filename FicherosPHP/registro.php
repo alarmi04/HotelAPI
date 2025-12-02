9f419a45c3f47d1bb1f972483751aee568de2f2b
ellope01 <algieba23@gmail.com>
2025-12-02 17:26:32 +0100

    formularios

---
+++ FicherosPHP/registro.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<h1>Crea una cuenta</h1>
+<form action="#" method="POST">
+    <p>Nombre</p>
+    <input type="text" name="nombre">
+    <p>Correo electronico</p>
+    <input type="email" name="correo">
+    <p>Contraseña</p>
+    <input type="password" name="clave">
+    <p>Telefono</p>
+    <input type="text" name="telefono">
+    <br>
+    <br>
+    <input type="submit" value="Registrarse">
+</form>
+
+
